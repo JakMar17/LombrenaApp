83c4adc64277c09718253f69fa16c31668d29699
JakMar17 <jakob.marusic17@gmail.com>
2020-06-10 12:24:10 +0200

    ADDED google maps style

--- lib/screens/maps/map.dart
+++ lib/screens/maps/map.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class MapOfSlovenia extends StatefulWidget {
   MapOfSlovenia({Key key}) : super(key: key);
@@ -11,13 +12,23 @@ class MapOfSlovenia extends StatefulWidget {
 }
 
 class _MapOfSloveniaState extends State<MapOfSlovenia> {
-  
+  String _mapStyle;
    Completer<GoogleMapController> _controller = Completer();
 
   static const LatLng _center = const LatLng(46.056946, 14.505751);
 
   void _onMapCreated(GoogleMapController controller) {
+    controller.setMapStyle(_mapStyle);
     _controller.complete(controller);
+  }
+
+  @override
+  void initState() { 
+    super.initState();
+    
+    rootBundle.loadString('assets/map_style/map_style.txt').then((string) {
+    _mapStyle = string;
+  });
   }
 
   @override
