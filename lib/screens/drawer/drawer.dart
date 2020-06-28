dac870e93e074dab65ee8b3476776aacc1682aeb
JakMar17 <jakob.marusic17@gmail.com>
2020-06-28 20:47:47 +0200

    ADDED scrollable drawer

--- lib/screens/drawer/drawer.dart
+++ lib/screens/drawer/drawer.dart
@@ -6,95 +6,100 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         color: Colors.transparent,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 40, left: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      CircleAvatar(
-                        //backgroundColor: Colors.black12,
-                        radius: 50,
-                        backgroundImage:
-                            AssetImage("assets/images/icon128.png"),
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            "MarelaApp",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontFamily: "Montserrat",
-                                fontSize: 24,
-                                letterSpacing: 0.8,
-                                fontWeight: FontWeight.w500),
-                          ),
-                          Text(
-                            "by MarelaTeam",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontFamily: "Montserrat",
-                                fontSize: 18,
-                                fontWeight: FontWeight.w200,
-                                letterSpacing: 0.6),
-                          )
-                        ],
-                      )
-                    ],
+        child: NotificationListener<OverscrollIndicatorNotification>(
+          onNotification: (OverscrollIndicatorNotification overscroll) {
+            overscroll.disallowGlow();
+          },
+          child: ListView(
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 40, left: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        CircleAvatar(
+                          //backgroundColor: Colors.black12,
+                          radius: 50,
+                          backgroundImage:
+                              AssetImage("assets/images/icon128.png"),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "MarelaApp",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: "Montserrat",
+                                  fontSize: 24,
+                                  letterSpacing: 0.8,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            Text(
+                              "by MarelaTeam",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: "Montserrat",
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w200,
+                                  letterSpacing: 0.6),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 100,
-                ),
-                buttonRow(Icons.cloud, "Vremenska napoved", () {
-                  Navigator.pop(context);
-                  Navigator.pushNamed(context, "/napovedi");
-                }),
-                buttonRow(Icons.wb_sunny, "Vremenske razmere", () {
-                  Navigator.pop(context);
-                  Navigator.pushNamed(context, "/postaje");
-                }),
-                buttonRow(Icons.textsms, "Tekstovna napoved", () {
-                  Navigator.pop(context);
-                  Navigator.pushNamed(context, "/napoved/tekst");
-                }),
-                buttonRow(WeatherIcons.water, "Vodotoki", () {
-                  Navigator.pop(context);
-                  Navigator.pushNamed(context, "/vodotoki");
-                }),
-                buttonRow(Icons.warning, "Opozorila", () {
-                  Navigator.pop(context);
-                  Navigator.pushNamed(context, "/warnings");
-                })
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                buttonRow(Icons.settings, "Nastavitve", () {
-                  Navigator.pushNamedAndRemoveUntil(
-                      context, "/settings", (r) => false);
-                  /* Navigator.pop(context);
-                  Navigator.pushNamed(context, "/settings"); */
-                }),
-                buttonRow(Icons.library_books, "O aplikaciji", () {
-                  Navigator.pop(context);
-                  Navigator.pushNamed(context, ("/about"));
-                }),
-                SizedBox(
-                  height: 20,
-                )
-              ],
-            )
-          ],
+                  SizedBox(
+                    height: 100,
+                  ),
+                  buttonRow(Icons.cloud, "Vremenska napoved", () {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, "/napovedi");
+                  }),
+                  buttonRow(Icons.wb_sunny, "Vremenske razmere", () {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, "/postaje");
+                  }),
+                  buttonRow(Icons.textsms, "Tekstovna napoved", () {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, "/napoved/tekst");
+                  }),
+                  buttonRow(WeatherIcons.water, "Vodotoki", () {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, "/vodotoki");
+                  }),
+                  buttonRow(Icons.warning, "Opozorila", () {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, "/warnings");
+                  })
+                ],
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.22),
+              Column(
+                children: <Widget>[
+                  buttonRow(Icons.settings, "Nastavitve", () {
+                    Navigator.pushNamedAndRemoveUntil(
+                        context, "/settings", (r) => false);
+                    /* Navigator.pop(context);
+                    Navigator.pushNamed(context, "/settings"); */
+                  }),
+                  buttonRow(Icons.library_books, "O aplikaciji", () {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, ("/about"));
+                  }),
+                  SizedBox(
+                    height: 20,
+                  )
+                ],
+              )
+            ],
+          ),
         ));
   }
 
